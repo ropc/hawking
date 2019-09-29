eca23b585167795c5723c3ddb7c5e1aadfa91af8
ropc <itslikeroar2412@gmail.com>
2019-09-28 22:44:35 -0400

    hold history and auto text to speech

--- hawking/ViewController.swift
+++ hawking/ViewController.swift
@@ -7,21 +7,58 @@
 //
 
 import Cocoa
+import AppKit
 
 class ViewController: NSViewController {
+    
+    @IBOutlet var realHistoryView: NSTextView!
+
+    let speechSynth = NSSpeechSynthesizer()
+    var lastLength = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        realHistoryView.delegate = self
+        realHistoryView.font = .systemFont(ofSize: 36)
+    }
+}
+
+extension ViewController: NSTextViewDelegate {
+
+    var lastLine: String? {
+        // this is kinda slow for large strings, should fix
+        return realHistoryView.string.split(separator: "\n").last.flatMap { String($0) }
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    func textViewDidChangeSelection(_ notification: Notification) {
+        let range = realHistoryView.selectedRange()
+        if range.length > 0, let text = realHistoryView.string.substring(with: range) {
+            speechSynth.say(text: String(text))
         }
     }
 
-
+    func textDidChange(_ notification: Notification) {
+        let allText = realHistoryView.string
+        let oldLastLength = lastLength
+        lastLength = allText.count
+        guard allText.last == "\n", let text = lastLine, oldLastLength < allText.count else { return }
+        speechSynth.say(text: text)
+    }
 }
 
+extension String {
+    func substring(with nsrange: NSRange) -> Substring? {
+        guard let range = Range(nsrange, in: self) else { return nil }
+        return self[range]
+    }
+}
+
+extension NSSpeechSynthesizer {
+    func say(text: String) {
+        let speakableText = text
+            .replacingOccurrences(of: "\\bidk\\b", with: "I D K", options: [.regularExpression, .caseInsensitive])
+        startSpeaking(speakableText)
+    }
+}
